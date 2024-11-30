416ad6f790c1de0ed31bf09f63f8805a73545ce8
mostafaMillensys <mostafa.Mahmoud@millensys.com>
2024-11-30 22:02:25 +0200

    Initial Commit

---
+++ JahezTask/JahezTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JahezTaskApp.swift
+//  JahezTask
+//
+//  Created by Mostafa Mahmoud on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct JahezTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
